d06c0360574fab662fb23bcbae52a825684d9540
kaija <kaija.chang@gmail.com>
2017-06-04 09:46:49 +0800

    support hook callback function

--- examples/proxy_example.js
+++ examples/proxy_example.js
@@ -1,5 +1,23 @@
 var named = require('../lib');
-var proxy = named.createProxy(['8.8.8.8', '168.95.1.1']);
+
+function b(d){
+    console.log(d);
+    r={}
+    r.answer = [{name: d, class: 1, type:1, ttl:300, address:'1.1.1.1'}];
+    return r;
+};
+
+function a(r){
+    console.log(r);
+    r.answer = [ { name: 'www.google.com',
+        class: 1,
+        type: 1,
+        ttl: 299,
+        address: '8.8.8.8' }];
+};
+
+var proxy = named.createProxy(['8.8.8.8', '168.95.1.1'], {before: b, after: a});
+
 proxy.listen(53, '127.0.0.1', function() {
-  console.log("Listen DNS on port 53");
+    console.log("Listen DNS on port 53");
 });
